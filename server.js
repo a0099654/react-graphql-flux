91229531683fd18683cc642e8dc63dad2f32235c
Stella Widyasari <stellaw@thoughtworks.com>
2017-12-12 16:18:38 +0800

    [Stella]Connect local mongodb to the application and change the mongo version

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 import express from 'express';
+import { MongoClient } from 'mongodb';
 
 let app = express();
+let db;
 
 app.use(express.static('public'));
 
-app.listen(3000);
+
+MongoClient.connect("mongodb://localhost:27017/rgrjs", (err, database) => {
+    if (err) throw err;
+
+    db = database;
+    app.listen(3000, () => { console.log('Listening on port 3000') });
+});
+
+app.get("/data/links", (req, res) => {
+    db.collection("links").find({}).toArray((err, links) => {
+        if (err) throw err;
+        res.json(links);
+    });
+})
