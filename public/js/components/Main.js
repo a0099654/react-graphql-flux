a7bebe202a561c0a037468c33c41444b292f66ed
Stella Widyasari <stellaw@thoughtworks.com>
2017-12-13 10:45:53 +0800

    [Stella]Linking store with the view by implementing method to set the state in the main class

--- public/js/components/Main.js
+++ public/js/components/Main.js
@@ -2,16 +2,38 @@ import React from 'react';
 import API from '../API';
 import LinkStores from '../stores/LinkStore';
 
+let _getAppState = () => {
+    return { links: LinkStores.getAll() }
+}
 export default class Main extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = _getAppState();
+        this.onChange = this.onChange.bind(this);
+    }
+
     componentDidMount() {
-        console.log('1 . here');
         API.fetchLinks();
+        LinkStores.on("change", this.onChange);
+    }
+
+    componentWillUnmount() {
+        LinkStores.removeListener("change", this.onChange);
+    }
+
+    onChange() {
+        this.setState(_getAppState());
     }
     render() {
+        let content = this.state.links.map(link => {
+            return <li key={link._id}>
+                <a href={link.url}>{link.title}</a>
+            </li>;
+        });
         return (
             <div><h3>links</h3>
                 <ul>
-                    <li>LALA</li>
+                    {content}
                 </ul>
             </div>
         );
