5f469561811c6c70ad81cff5676a69e02c15cbb5
Stella Widyasari <stellaw@thoughtworks.com>
2017-12-13 13:54:14 +0800

    [Stella]Adding graphql to react application

--- server.js
+++ server.js
@@ -19,10 +19,3 @@ MongoClient.connect("mongodb://localhost:27017/rgrjs", (err, database) => {
 
     app.listen(3000, () => { console.log('Listening on port 3000') });
 });
-
-app.get("/data/links", (req, res) => {
-    db.collection("links").find({}).toArray((err, links) => {
-        if (err) throw err;
-        res.json(links);
-    });
-})
